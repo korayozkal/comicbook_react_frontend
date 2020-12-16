20dbd2a817234f678254d01024328d519e83ae04
kryzkl <46037287+korayozkal@users.noreply.github.com>
2020-12-15 20:21:18 -0800

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "react-thunk";
+import { composeWithDevTools } from 'redux-devtools-extension';
+//this replaces window.__redux_devtools
+
+const store = createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk))
+)
+
+
+
+
+
+
+
 import './index.css';
-import App from './App';
+import App from 'components/App';
+import 'components/app.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
